ac8c31d4d63302a8a38c2aa21bb6640e80568ce2
Elaman Imashov <1479020+elaman@users.noreply.github.com>
2024-01-18 16:19:04 +0600

    Add RDS

--- main.tf
+++ main.tf
@@ -152,3 +152,15 @@ resource "aws_route53_record" "drpler" {
     zone_id                = aws_lb.drpler.zone_id
   }
 }
+
+resource "aws_db_instance" "db_instance" {
+  allocated_storage = 20
+  storage_type = "standard"
+  engine = "mysql"
+  engine_version = "8.0.35"
+  name = "drpler" # deprecated?
+  username = "example"
+  password = "example"
+  skip_final_snapshot = true
+  instance_class = "db.t2.micro"
+}
