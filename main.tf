b5cc610a801a55212347667e652051654d7713ed
Elaman Imashov <1479020+elaman@users.noreply.github.com>
2024-01-18 15:56:19 +0600

    Use route53 to connect to my domain

--- main.tf
+++ main.tf
@@ -20,8 +20,7 @@ data "aws_subnet_ids" "default_subnet" {
 }
 
 # Generally format in defining resource is "TYPE" "CUSTOM NAME"
-# Naming things is HARD!
-resource "aws_instance" "drupal_1" {
+resource "aws_instance" "drpler_1" {
   ami             = "ami-04075458d3b9f6a5b"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instances.name]
@@ -31,7 +30,7 @@ resource "aws_instance" "drupal_1" {
     python3 -m http.server 8080 &
     EOF
 }
-resource "aws_instance" "drupal_2" {
+resource "aws_instance" "drpler_2" {
   ami             = "ami-04075458d3b9f6a5b"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instances.name]
@@ -76,7 +75,7 @@ resource "aws_security_group_rule" "allow_lb_http_outbound" {
 
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.load_balancer.arn
+  load_balancer_arn = aws_lb.drpler.arn
 
   protocol = "HTTP"
   port = 80
@@ -109,11 +108,11 @@ resource "aws_lb_target_group" "instances" {
 }
 resource "aws_lb_target_group_attachment" "drupal_1" {
   target_group_arn = aws_lb_target_group.instances.arn
-  target_id        = aws_instance.drupal_1.id
+  target_id        = aws_instance.drpler_1.id
 }
 resource "aws_lb_target_group_attachment" "drupal_2" {
   target_group_arn = aws_lb_target_group.instances.arn
-  target_id        = aws_instance.drupal_2.id
+  target_id        = aws_instance.drpler_1.id
 }
 resource "aws_lb_listener_rule" "instances" {
   listener_arn = aws_lb_listener.http.arn
@@ -130,9 +129,26 @@ resource "aws_lb_listener_rule" "instances" {
     target_group_arn = aws_lb_target_group.instances.arn
   }
 }
-resource "aws_lb" "load_balancer" {
-  name = "drupal-lb"
+resource "aws_lb" "drpler" {
+  name = "drpler"
   load_balancer_type = "application"
   subnets = data.aws_subnet_ids.default_subnet.ids
   security_groups = [aws_security_group.lb.id]
+}
+
+
+
+resource "aws_route53_zone" "drpler" {
+  name = "drpler.com"
+}
+resource "aws_route53_record" "drpler" {
+  name    = "drpler.com"
+  type    = "A"
+  zone_id = aws_route53_zone.drpler.zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.drpler.dns_name
+    zone_id                = aws_lb.drpler.zone_id
+  }
 }
